87b838e1e6718e2b3f61bbfbb7cbff96a2baf64d
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-04-01 20:18:04 -0700

    adding global vars to js file

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Assignment code here 
-
+var generateBtn = document.querySelector("#generate");
+var lowerLetter = confirm("Do you want lower case letters?")
+var upperLetter = confirm("Do you want upper case letters?")
+var num = confirm("Do you want numbers?")
+var special = confirm("Do you want special characters?")
+var passwordLength = confirm("Pick a password length between 8 and 128 characters.")
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Writing conditional else/if
+if (passwordLength >= 8 && <= 128);{
+  function generatePassword() {
+    
+  }
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,7 +23,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button

c8d1a93a0ab7b1e4a363b22a2e13cc0f98853375
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-04-01 20:33:18 -0700

    added else if statements for what options user selects

--- script.js
+++ script.js
@@ -3,16 +3,42 @@ var generateBtn = document.querySelector("#generate");
 var lowerLetter = confirm("Do you want lower case letters?")
 var upperLetter = confirm("Do you want upper case letters?")
 var num = confirm("Do you want numbers?")
-var special = confirm("Do you want special characters?")
+var spChar = confirm("Do you want special characters?")
 var passwordLength = confirm("Pick a password length between 8 and 128 characters.")
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Writing conditional else/if
-if (passwordLength >= 8 && <= 128);{
+if (passwordLength >= 8 && passwordLength <= 128);{
   function generatePassword() {
+    var undefinedString = "";
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVQXYZ';
+    var symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    var numbers = '0123456789';
+
+    if (lowerLetter === true) {
+      undefinedString += lowercase
+    };
+
+    if (upperLetter === true) {
+      undefinedString += uppercase
+    };
+
+    if (num === true) {
+      undefinedString += numbers
+    };
     
+    if (spChar === true) {
+      undefinedString += symbols
+    };
+
+    return password;
+
+
+
+
   }
 }
 
